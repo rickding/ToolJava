0fc8bed4e29d8f6fc16456e4902cb4245dca10c7
dingxl <dingxl18@qq.com>
2018-12-06 14:00:31 +0800

    init doc parser

---
+++ doc-parser/src/main/java/com/tool/doc/parser/App.java
@@ -0,0 +1,10 @@
+package com.tool.doc.parser;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello doc parser!");
+    }
+}
---
+++ doc-parser/src/test/java/com/tool/doc/parser/AppTest.java
@@ -0,0 +1,19 @@
+package com.tool.doc.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testApp() {
+        App.main(null);
+    }
+}
